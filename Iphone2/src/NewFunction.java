1bc760257d6d8d26085af42574146d8d51020877
yoyoyo25 <yshrck05@outlook.jp>
2019-05-23 19:21:11 +0900

    0523

--- Iphone2/src/NewFunction.java
+++
@@ -1,9 +0,0 @@
-//インターフェース：ルール作成
-public interface NewFunction {
-	
-	public abstract void call();
-	public abstract void mail();
-	public abstract void photo();
-	public abstract void internet();
-	
-}
